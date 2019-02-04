16cf92b12f22de41a56c88e7b8c93fa7dfcce8cc
freitzzz <1160907@isep.ipp.pt>
2019-02-04 18:27:20 +0000

    Added UIDart class which holds a user interface information/strings

---
+++ lib/utils/UIData.dart
@@ -0,0 +1,17 @@
+///Class that holds information regarding the user interface
+class UIData{
+    //General information
+    static const String APP_NAME="Flutter Spike";
+
+    //Application routes
+    
+    /**
+     * String with the login route
+     */
+    static const String LOGIN_ROUTE="/login";
+
+    /**
+     * String with the home route
+     */
+    static const String HOME_ROUTE="/home";
+}
